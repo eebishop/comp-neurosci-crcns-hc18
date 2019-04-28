27fa93515bfa5c8c617c13c55866dec8dbc230bd
Emily Bishop <Embo@d-172-25-143-222.dhcp.virginia.edu>
2019-04-28 15:41:01 -0400

    Updated load_data again so it actually works

--- src/load_data.py
+++ src/load_data.py
@@ -1,56 +1,46 @@
 # loading data
 
-import pandas as pd
 import glob
-from os import path 
+from os import path
+import numpy as np
 
-def make_arr(fname):
-    """
-    The purpose of this function is to create an array from the data in a file in flat ascii format
+x = {}
 
-    ONLY WORKS FOR CLU AND RES FILES
-    """
-    arr = []
-    with open(fname, "rt") as fp:
-        arr.append(np.fromstring(line, sep = "\n") for line in fp)
-
-def make_arr_pos(pos_file):
-    """
-    The purpose of this function is to create an array from the data in the .pos files
-    """
-    arr = []
-    with open(fname, "rt") as fp:
-        for line in fp:
-            arr.append([line[0, 2], line[3, 6]])
-
-def flatten_data():
-    """
-    The purpose of this function is to flatten the data that is inside the .pos, .res, .clu
-
-    This function requires iterating through all of the .pos, .res, and .clu files and creating a nested format of all of the data
-
-    For reference, the .pos files contain x and y positions of the rat in terms of the video, .res is the samples, and .clu is the clusters and tetrodes 
-
-    return: an array of the flattened data
-    """
-    data = {}
-    for train in glob.glob("data/trains/*"):
-        train_name = path.basename(train)
-        for file in glob.glob("data/trains/train/*"):
-            if "pos" in file:
-                data_1 = make_arr_pos(file)
-                file_type = path.basename(file)[0]
-                tetrode_num = "NA"
-            else:
-                file_type = path.basename(file)[0]
-                tetrode_num = path.basename(file)[1]
+for t in glob.glob("/Users/Embo/comp-neurosci-crcns-hc18/data/trains/*"):
+    train_name = t[-3:]
+    for file in glob.glob(t + "/*"):
+        train = []
+        file_type = path.basename(file)[19:22]
+        if file_type in ["clu", "res"]:
+            tetrode_num = path.basename(file)[23:]
+            file_name = file_type + tetrode_num
+            data_arr = []
+            with open(file, "rt") as fp:
                 for line in fp:
-                    data_1 = make_arr(file)
-            ndata = {"file": file_type, "data": data_1, "tetrode_num" : tetrode_num}
-            data[train_name] = ndata
-    df = pd.DataFrame(data)
-    return df
-
+                    line = line.strip()
+                    data_arr.append(line)
+            spike_data = {"file_name": file_name, "tetrode_num": tetrode_num, "data": data_arr}
+            train.append(spike_data)
+        elif file_type == "pos":
+            positions = []
+            with open(file, "rt") as fp:
+                for line in fp:
+                    line = line.strip().split("\t")
+                    positions.append(line)
+            pos_data = {"positions": positions}
+            train.append(pos_data)
+        elif file_type == "cat":
+            file_type = "cat.evt"
+            phases = []
+            with open(file, "rt") as fp:
+                for line in fp:
+                    line = line.strip()
+                    phases.append(line)
+            phase_data = {"phases": phases}
+            train.append(phase_data)
+    x[train_name] = train
+        
+print(x)
 
 
 
