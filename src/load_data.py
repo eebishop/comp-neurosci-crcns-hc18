711e7d0e95f622ce04bea771508fcf5d9fbaed13
Emily Bishop <Embo@Emilys-MacBook-Air-5.local>
2019-05-01 11:57:39 -0400

    updated load_data.py only needs documentation

--- src/load_data.py
+++ src/load_data.py
@@ -5,79 +5,84 @@ from os import path
 import numpy as np
 import pandas as pd
 
+# Note: Must change file name of all files to exclude date for make_dict() to work properly
+
 def make_dict():
-"""
-This function creates a nested dict with all of the trains and their asosciated files (including res, clu, pos, and cat.evt)
-First level is trains and second level is all of the files (in the form of arrays)
-The function calls other functions
-
-Parameter: There are no parameters
-
-Return: Returns the nested dictionary
-"""
     d = {} # First level dictionary
     for t in glob.glob("/Users/Embo/comp-neurosci-crcns-hc18/data/trains/*"):
         train_name = t[-3:]
         train = {} # Second level dictionary
-        train["positions"] = load_pos(t + "Train-" + train_name + ".pos")
-        train["phases"] = load_phases(t + "Train-" + train_name + ".cat.evt")
+        train["positions"] = load_pos(t + "/Train-" + train_name + ".pos")
+        train["phases"] = load_phases(t + "/Train-" + train_name + ".cat.evt")
         for file in glob.glob(t + "/*"): # More complicated code for the individual clu and res files
-            file_type = path.basename(file)[19:22]
+            file_type = path.basename(file)[10:13]
             if file_type in ["res", "clu"]:
-                tetrode_num = path.basename(file)[23:]
-                train[file_type+tetrode_num] = load_clu_res(t + "Train-" + train_name + "." file_type + "." + tetrode_num)
+                tetrode_num = path.basename(file)[14:]
+                train[file_type+tetrode_num] = load_clu_res(t + "/Train-" + train_name + "." + file_type + "." + tetrode_num)
         d[train_name] = train
     return d
 
 def load_pos(fname):
-"""
-This function iterates through the pos file of a specific train and return a nested list with positions for each line in the file
-
-Parameter: Accepts a file name that is the pos file for a specific train
-
-Return: A nested list
-"""
     positions = []
     with open(fname, "rt") as fp:
         for line in fp:
-            line = int(line.strip().split("\t"))
+            line = line.strip().split("\t")
+            line = map(int, line)
             positions.append(line)
     return positions
 
 
 def load_phases(fname):
-"""
-This function iterates through the cat.evt file of a specific train and returns a nested list with positions for each line in the file
-
-Parameter: Accepts a file name that is the cat.evt file for a specific train
-
-Return: A nested list
-"""
     phases = []
     with open(fname, "rt") as fp:
         for line in fp:
-            line = float(line.strip().split()[0])
-            phases.append(line)
+            line = line.strip().split()
+            ret = []
+            ret.append(line[0])
+            ret.append(line[1])
+            ret.append(line[3][22:])
+            phases.append(ret)
     return phases
 
 
 def load_clu_res(fname):
-"""
-This function iterates through a clu or res file for a specific train and returns a nested list with the data for each line of the file
-
-Parameter: Accepts a file name that is ta clu or res file for a specific train
-
-Return: A list
-"""
-   data_arr = []
-        with open(fname, "rt") as fp:
-            for line in fp:
-                line = int(line.strip())
-                data_arr.append(line) 
+    data_arr = []
+    with open(fname, "rt") as fp:
+        for line in fp:
+            data_arr.append(int(line)) 
     return data_arr
 
 # Testing
 
+testing_mode = False
 
+if testing_mode:
+
+    # Test load_pos
+
+    pos_test = load_pos("/Users/Embo/comp-neurosci-crcns-hc18/data/trains/train.242/Train-242.pos")
+    print(pos_test[:10])
+
+    # Test load_clu_res
+
+    clu_test = load_clu_res("/Users/Embo/comp-neurosci-crcns-hc18/data/trains/train.242/Train-242.clu.2") 
+    print(clu_test[:10])
+
+    res_test = load_clu_res("/Users/Embo/comp-neurosci-crcns-hc18/data/trains/train.242/Train-242.res.2") 
+    print(res_test[:10])
+
+    # Test load_phases
+
+    phase_test = load_phases("/Users/Embo/comp-neurosci-crcns-hc18/data/trains/train.242/Train-242.cat.evt")
+    print(phase_test[:10])
+
+    #  Test make_dict
+
+    dict_test = make_dict()
+    print(dict_test.keys())
+
+df = pd.DataFrame(make_dict())
+df.transpose()
+print(df)
 
 
