1f0e4b9e3ad2a32c9633479c814a3a3703d3c003
Emily Bishop <Embo@d-172-25-220-119.dhcp.virginia.edu>
2019-05-03 13:06:33 -0400

    updated load_data

--- data/README.md
+++ data/README.md
@@ -1,14 +1,11 @@
 To retrieve data set go to CRCNS website and find the hc-18 data set. From there:
 
-		1. Download each .tar.gz from the hc-18 data from the CRCNS website
+		1. Download the Train-242 tar.gz file 
 		2. Move relevant files into the data folder
-			a. These files are: .clu#, .pos#, .res#, .cat.evt and .xml
-		3. Organize the data into a folder for each session(i.e. train)
-		4. Change the file names to exclude the date. For example:
-			Before: Train-242-20140617.pos
-			After: Train-242.pos
+			a. These files are: .clu#, .pos#, and .xml
+		3. Change the file names to exclude the date. For example:
+			Before: Train-242-20140617.clu.12
+			After: Train-242.clu.12
 			This is important as the functions made to read data is looking for file names
 			in this format.
-			We have written function that will do this for you, please 
-			use it.
-		5. We will not be using the following files: .fet#, .lfp, .spk#
+		4. We will not be using the following files: .fet#, .lfp, .spk#, .pos, and .cat.evt
--- src/load_data.py
+++ src/load_data.py
@@ -4,7 +4,7 @@ import glob
 from os import path
 import os
 import numpy as np
-# import pandas as pd
+import pandas as pd
 
 # Note: Must change file name of all files to exclude date for make_dict() to work properly
 
@@ -41,7 +41,7 @@ def change_name_train(train_name):
 
 def make_dict():
     """
-    This function creates anested dictionary of all of the files in the dataset. Top layer of dictionary is of the indivdual
+    This function creates a nested dictionary of all of the files in the dataset. Top layer of dictionary is of the indivdual
     trains. Second level dictionary is the all of the files within each train (e.g. clu, res, pos, etc.). This function 
     calls other functions in this script in order to make the dictionary
 
@@ -49,105 +49,21 @@ def make_dict():
 
     Return: The nested dictionary
     """
-    d = {} # First level dictionary
-    for t in glob.glob("/Users/Embo/comp-neurosci-crcns-hc18/data/trains/*"):
-        train_name = t[-3:]
-        train = {} # Second level dictionary
-        train["positions"] = load_pos(t + "/Train-" + train_name + ".pos")
-        train["phases"] = load_phases(t + "/Train-" + train_name + ".cat.evt")
-        for file in glob.glob(t + "/*"): # More complicated code for the individual clu and res files
-            file_type = path.basename(file)[10:13]
-            if file_type in ["res", "clu"]:
-                tetrode_num = path.basename(file)[14:]
-                train[file_type+tetrode_num] = load_clu_res(t + "/Train-" + train_name + "." + file_type + "." + tetrode_num)
-        d[train_name] = train
-    return d
-
-def load_pos(fname):
-    """
-    This function opens a specific pos file and reads the data into an array format. It will
-    be a nested list where each item in the top layer list is an array of two positions. For example:
-    [[3, 4], [1,2]]
-
-    Parameter: the path of the file to open and read
-
-    Return: Nested list with position data
-    """
-    positions = []
-    with open(fname, "rt") as fp:
-        for line in fp:
-            line = line.strip().split("\t")
-            line = map(int, line)
-            positions.append(line)
-    return positions
+    t = "/Users/Embo/comp-neurosci-crcns-hc18/data/train.242"
+    train = {}
+    for clufile in glob.glob(t + "/*clu*"):
+        print(clufile)
+        tetrode_num = int(clufile.split(".")[-1])
+        train[tetrode_num] = load_spikes(clufile)
+    return train
 
 
-def load_phases(fname):
-    """
-    This function opens a specific cat.evt file and reads the data into an array format. It will
-    be a nested list where each item in the top layer list is an array of three items: the time value,
-    whether it denotes a beginning or end, and the phase it represents. For example:
-    [[0, 'beginnning', 'SleepBaseline']]
-
-    Parameter: the path of the file to open and read
-
-    Return: Nested list with phase data
-    """
-    phases = []
-    with open(fname, "rt") as fp:
-        for line in fp:
-            line = line.strip().split()
-            ret = []
-            ret.append(line[0])
-            ret.append(line[1])
-            ret.append(line[3][22:])
-            phases.append(ret)
-    return phases
-
-
-def load_clu_res(fname):
-    """
-    This function opens a specific clu or res file and reads the data into an array format. The first value 
-    in the array represents SOMETHING IMPORTANT 
-
-    Parameter: the path of the file to open and read
-
-    Return: List with data
-    """
-    data_arr = []
-    with open(fname, "rt") as fp:
-        for line in fp:
-            data_arr.append(int(line)) 
-    return data_arr
-
-
-# Testing
-
-testing_mode = False
-
-if testing_mode:
-
-    # Test load_pos
-
-    pos_test = load_pos("/Users/Embo/comp-neurosci-crcns-hc18/data/trains/train.242/Train-242.pos")
-    print(pos_test[:10])
-
-    # Test load_clu_res
-
-    clu_test = load_clu_res("/Users/Embo/comp-neurosci-crcns-hc18/data/trains/train.242/Train-242.clu.2") 
-    print(clu_test[:10])
-
-    res_test = load_clu_res("/Users/Embo/comp-neurosci-crcns-hc18/data/trains/train.242/Train-242.res.2") 
-    print(res_test[:10])
-
-    # Test load_phases
-
-    phase_test = load_phases("/Users/Embo/comp-neurosci-crcns-hc18/data/trains/train.242/Train-242.cat.evt")
-    print(phase_test[:10])
-
-    #  Test make_dict
-
-    dict_test = make_dict()
-    print(dict_test.keys())
-    print(dict_test["261"].keys())
-    print(dict_test["261"]["phases"])
+def load_spikes(clufile):
+    print("hi")
+    sampling_rate = 32552.083 
+    resfile = clufile.replace("clu", "res")
+    cludata = np.loadtxt(clufile)[1:]
+    resdata = (np.loadtxt(resfile) / sampling_rate)
+    # data_dict = {"cludata": cludata, "resdata": resdata}
+    df = pd.DataFrame({"cluster": cludata, "time (ms)": resdata})
+    return df
